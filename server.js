09b6bbb0a3280cdfcb1fbadd54d41bb2df256e05
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-27 17:55:32 +0530

    Created contactRoutes.js for routes of api.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 6000;
 
+app.use('/api/contacts',require('./routes/contactRoutes'))
+
 app.listen(port,() => {
     console.log("Hello there i am in "+port);
 })

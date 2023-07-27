4c6c54bebd9887f589fdb607b22a613b660490b2
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-27 22:43:43 +0530

    Installed express-async-handler as for promises to be resolved we need async function and for catching error in async function we need try-catch block so to avoid changing the functions I installed.

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,14 +1,17 @@
 //@desc Get all contacts
 //@route GET /api/contacts
+
+const asyncHandler = require("express-async-handler")
+
 //@access Public
-const getContacts = (req,res)=>{
+const getContacts = asyncHandler (async (req,res)=>{
     res.status(200).json({ message: "Get all contacts." })
-}
+});
 
 //@desc Create contact
 //@route POST /api/contacts/
 //@access Public
-const createContact = (req,res)=>{
+const createContact = asyncHandler (async (req,res)=>{
     console.log("The request body is", req.body);
     const {name, email, phone} = req.body;
     if( !name || !email || !phone){
@@ -16,28 +19,28 @@ const createContact = (req,res)=>{
         throw new Error("All fields are necessary!!");
     }
     res.status(201).json({ message: "Create contacts." })
-}
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access Public
-const getContact = (req,res)=>{
+const getContact = asyncHandler (async (req,res)=>{
     res.status(200).json({ message: `Get contact of ${req.params.id}` })
-}
+});
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler (async (req,res)=>{
     res.status(200).json({ message: `Update contact of ${req.params.id}` })
-}
+});
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler (async (req,res)=>{
     res.status(200).json({ message: `Delete contact for ${req.params.id}` })
-}
+});
 
 module.exports = {
     getContacts,

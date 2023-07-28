e45bb19d9f0c150267eed3a24219f181f431cf6b
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-28 20:55:29 +0530

    Created userModel.js, added logic in userController.js for user to register and added bcrypt for password hassing.

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,34 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 
 //@desc Register User
 //@route POST /api/user/register
 //@access Public
 const registerUser = asyncHandler (async (req, res) => {
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All feilds are required to register.")
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User with this email already exist.");
+    }
+    const hassedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        username, email, password: hassedPassword,
+    });
+    console.log(`User Created ${user}`);
+    if(user){
+        res.status(201).json({_id: user.id, email: email})
+    }else{
+        res.status(400);
+        throw new Error("User data not valid.")
+    }
+
     res.json({message: "Register user"});
 });
 

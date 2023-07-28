34b775f4fba45ba51b7482fd9e029629034e4838
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-28 22:50:59 +0530

    Installed jsonwebtoken for get a token to the login user to protect the user information that'll be there in the access token by using jsonwebtoken.

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 
@@ -37,6 +38,25 @@ const registerUser = asyncHandler (async (req, res) => {
 //@route POST /api/user/login
 //@access Public
 const loginUser = asyncHandler (async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All the fields are mandatory!!");
+    }
+    const user = await User.findOne({ email });
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            },
+        }, process.env.ACCESS_TOKEN_SECERT, {expiresIn: "10m"});
+        res.status(200).json({accessToken});
+    }else {
+        res.status(401);
+        throw new Error("Invalid credentials!! Please check the credentials.")
+    }
     res.json({message: "Login user"});
 });
 
